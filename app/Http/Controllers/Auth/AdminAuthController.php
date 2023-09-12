288f96e42ab4aa67e610b4ff63fbace2f140b2d9
AmerDawoood <amermadawood@gmail.com>
2023-09-12 08:44:23 +0300

    start in multi guard

--- app/Http/Controllers/Auth/AdminAuthController.php
+++ app/Http/Controllers/Auth/AdminAuthController.php
@@ -3,9 +3,84 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
 
 class AdminAuthController extends Controller
 {
-    //
+    public function adminLogin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('admin.login.form')
+            ->withErrors($validator)
+            ->withInput();
+
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::guard('admin')->attempt($credentials)) {
+            $user = Auth::guard('admin')->user();
+            return redirect()->route('dashboard');
+
+
+        } else {
+            // toastr()->error('Oops! Something went wrong!');
+
+            return redirect()->route('admin.login.form');
+        }
+    }
+
+
+
+    public function adminRegister(Request $request){
+
+
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:admins',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+
+            return redirect()->route('admin.register.form')
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        $admin = Admin::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+
+        // toastr()->success('Admin Created Successfuly');
+
+        return redirect()->route('admin.login.form');
+
+    }
+
+
+
+    public function adminLogout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login.form');
+    }
 }
--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -3,12 +3,67 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 class UserAuthController extends Controller
 {
     public function login()
     {
         return view('Auth.login');
     }
+
+
+
+
+public function userLogin(Request $request){
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        return redirect()->route('user.dashboard');
+    }
+
+    // toastr()->error('Invalid credentials');
+
+    return redirect()->route('user.login.form');
+
+}
+
+
+public function userRegister(Request $request){
+
+    $validator = Validator::make($request->all(), [
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:6|confirmed',
+        'type'=>'required',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect()->route('user.register.form')
+            ->withErrors($validator)
+            ->withInput();
+    }
+
+    $user = User::create([
+        'name' => $request->input('name'),
+        'email' => $request->input('email'),
+        'password' => bcrypt($request->input('password')),
+        'type' => $request->input('type'),
+
+    ]);
+
+    // toastr()->success('User Registered Successfully');
+
+    return redirect()->route('user.login.form');
+
+}
+
+
+public function logoutUser()
+{
+    Auth::logout();
+    return redirect()->route('user.login.form');
+}
 }
